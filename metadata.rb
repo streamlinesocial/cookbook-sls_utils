80e5d4423aabe21c465ec9c6d5b3db6a130de438
Chris <chris@buryta.com>
2012-08-06 09:12:25 -0400

    added default apache2 site recipe to use srv folder

--- metadata.rb
+++ metadata.rb
@@ -9,6 +9,7 @@ version           "0.0.0"
 end
 
 recipe "sls_utils", "Empty recipe."
+recipe "sls_utils::apache2_default", "Enables apache with a custom default site to serve the /srv folder."
 recipe "sls_utils::port_http", "Opens port 80 in the iptables."
 recipe "sls_utils::port_ssh", "Opens port 22 in the iptables."
 recipe "sls_utils::deploy_key", "Creates a private key for the apache user for site deployments."
