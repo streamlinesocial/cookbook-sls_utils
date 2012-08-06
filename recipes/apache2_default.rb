4c374a967dc7ad773766634ffdd22a8e8af0227a
Chris <chris@buryta.com>
2012-08-06 10:36:01 -0400

    ensure restart after template, remove block of Indexes in default apache site

--- recipes/apache2_default.rb
+++ recipes/apache2_default.rb
@@ -1,10 +1,3 @@
-# enable on boot, and ensure server is started
-%w{ apache2 }.each do |service_name|
-  service "#{service_name}" do
-    action [ :enable, :start ]
-  end
-end
-
 # custom default site template to serve default site from /srv folder
 template "#{node['apache']['dir']}/sites-available/default" do
   source "default_site.erb"
@@ -17,4 +10,11 @@ end
 # ensure we enabled the defaut
 apache_site "default" do
   enable true
+end
+
+# enable on boot, and ensure server is started
+%w{ apache2 }.each do |service_name|
+  service "#{service_name}" do
+    action [ :enable, :start ]
+  end
 end
