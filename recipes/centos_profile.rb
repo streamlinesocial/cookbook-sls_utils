bd30115dda28e6a71d6d842d1b18e88a0435d9fd
Chris <chris@buryta.com>
2012-08-27 11:41:51 -0400

    add recipe to ensure /usr/sbin is in the PATH for the chef-client and chef-solo runs

---
+++ recipes/centos_profile.rb
@@ -0,0 +1,33 @@
+# ensure the paths for the centos box are properlly setup for the /usr/sbin folder, some recipies will fail otherwise
+
+# http://serverfault.com/questions/102932/adding-a-directory-to-path-in-centos
+# echo 'pathmunge /usr/lib/ruby-enterprise/bin' > /etc/profile.d/ree.sh
+# chmod +x /etc/profile.d/ree.sh
+# 1. add usr/sbin file to the profile.d folder
+# 2. ensure the file is executable
+# 3. run the file to trigger an path update now
+# script "add_usr_sbin_to_path" do
+#   interpreter "bash"
+#   user "root"
+#   cwd "/tmp"
+#   code <<-EOH
+#   pathmunge '/usr/sbin' > /etc/profile.d/usr_sbin_path.sh
+#   chmod +x /etc/profile.d/usr_sbin_path.sh
+#   /etc/profile
+#   EOH
+# end
+#
+#
+# execute "/etc/profile" do
+#   action :run
+# end
+
+script "add_usr_sbin_to_path" do
+  interpreter "bash"
+  user "root"
+  cwd "/tmp"
+  code <<-EOH
+  chmod +x /etc/profile
+  /etc/profile
+  EOH
+end
